e61da009d72ad07b6a5b2112e6b59983442b4f45
Luis Guerrero <158310573+LuisGuerrero7@users.noreply.github.com>
2024-07-13 01:52:28 +0000

    Domain.2

--- src/app.js
+++ src/app.js
@@ -5,7 +5,15 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+pronoun.forEach(proun => {
+    adj.forEach(ad => {
+        noun.forEach(no => {
+            console.log(proun + ad + no + ".com");
+        });
+    });
+});
+
